0bd6cb956eff19cc0d8871df234e507a6191df19
borislav nikolov <jack@sofialondonmoskva.com>
2014-09-10 22:46:36 +0200

    wip

--- dotfiles/emacs.d/init.el
+++ dotfiles/emacs.d/init.el
@@ -15,27 +15,10 @@
 (setq inhibit-startup-message t)
 (setq mouse-yank-at-point t)
 (blink-cursor-mode 0)
-(add-to-list 'custom-theme-load-path "~/.emacs.d/emacs-color-theme-solarized")
-(load-theme 'wombat t)
+(load-theme 'tango t)
 (add-to-list 'load-path "~/.emacs.d/lisp/")
-(load-file "~/.emacs.d/lisp/go-mode.el")
-(set-face-attribute 'default nil :height 110)
+(set-face-attribute 'default nil :height 150)
 (global-set-key [(control h)] 'delete-backward-char)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" "e890fd7b5137356ef5b88be1350acf94af90d9d6dd5c234978cd59a6b873ea94" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" default)))
- '(tramp-use-ssh-controlmaster-options nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
 (defun dos2unix ()
   "Replace DOS eolns CR LF with Unix eolns CR"
   (interactive)
@@ -180,3 +163,8 @@
 (local-set-key (kbd "M-k") 'my-mark-word)
 
 (local-set-key (kbd "M-j") 'my-mark-word-backward)
+
+(setq mac-option-key-is-meta nil
+      mac-command-key-is-meta t
+      mac-command-modifier 'meta
+      mac-option-modifier 'none)
