bbeafffd182a826acb1d41c44199138208651ae5
borislav nikolov <borislav.nikolov@booking.com>
2014-10-10 12:39:20 +0200

    Update can.c

--- broken/can.c
+++ broken/can.c
@@ -1,4 +1,8 @@
 //- adding _GNU_SOURCE makes it work with less than 1_000_000 iterations
+// because the malloc in canonicalize_file_name has address > 2^32
+// and without _GNU_SOURCE the function is not declared so return value is assumed INT
+// and basically is mangling the address
+
 //#define _GNU_SOURCE
 #include <stdlib.h>
 #include <stdio.h>
