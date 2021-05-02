1db22ccae140a48dc55ec161da1271da49400dcc
borislav nikolov <jack@sofialondonmoskva.com>
2021-05-02 17:21:42 -0400

    Update keymap.c

--- keymap.c
+++ keymap.c
@@ -41,7 +41,7 @@ const uint16_t keymaps_default[][MATRIX_ROWS][MATRIX_COLS] =
 			KC_ESC,  KC_F1,     KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,  KC_DEL,
 			_______, _______,   _______, _______, _______, _______, _______, _______, KC_PAUS,  KC_SLCK,  KC_PSCR,  KC_UP,    _______, _______,
 			_______, KC_VOLD,   KC_VOLU, KC_MUTE, _______, _______, _______, _______, KC_HOME,  KC_PGUP,  KC_LEFT,  KC_RIGHT, _______,
-			_______,    M(1),      M(2), _______, _______, _______, _______, _______, KC_END,   KC_PGDN,  KC_DOWN,  _______,
+			_______, _______,   _______, _______, _______, _______, _______, _______, KC_END,   KC_PGDN,  KC_DOWN,  _______,
 			_______, _______,   _______, _______, _______, _______, _______, _______
 			),
    [3] = LAYOUT_60_ansi(
@@ -49,7 +49,7 @@ const uint16_t keymaps_default[][MATRIX_ROWS][MATRIX_COLS] =
 			_______, _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
 			_______, _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
 			_______, _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-			KC_PGDN, _______,  _______, _______, _______, _______, _______, _______			
+			_______, _______,  _______, _______, _______, _______, _______, _______			
 			),
 
 #else
@@ -103,16 +103,39 @@ bool swap(uint8_t mod, uint8_t from, uint8_t to, bool pressed) {
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch(keycode) {
-  case KC_G: return swap(MOD_LCTL, KC_G, KC_ESC,  record->event.pressed); break;
-  case KC_J: return swap(MOD_LCTL, KC_J, KC_ENT,  record->event.pressed); break;
-  case KC_H: return swap(MOD_LCTL, KC_H, KC_BSPC, record->event.pressed); break;
+  uint8_t layer = biton32(layer_state);
+  if (layer == 3) {
+    switch(keycode) {
+    case KC_G: return swap(MOD_LCTL, KC_G, KC_ESC,  record->event.pressed); break;
+    case KC_J: return swap(MOD_LCTL, KC_J, KC_ENT,  record->event.pressed); break;
+    case KC_H: return swap(MOD_LCTL, KC_H, KC_BSPC, record->event.pressed); break;
+    case KC_P: return swap(MOD_LCTL, KC_P, KC_UP,   record->event.pressed); break;
+    case KC_N: return swap(MOD_LCTL, KC_N, KC_DOWN, record->event.pressed); break;
+    case KC_A: return swap(MOD_LCTL, KC_A, KC_HOME, record->event.pressed); break;
+    case KC_D: return swap(MOD_LCTL, KC_D, KC_DEL,  record->event.pressed); break;
+    case KC_E: return swap(MOD_LCTL, KC_E, KC_END,  record->event.pressed); break;
+    case KC_V: {
+      uint8_t m = get_mods();
+      if (m == MOD_LALT || saved_mod == MOD_LALT) {
+	return swap(MOD_LALT, KC_V, KC_PGUP,  record->event.pressed);
+      } else if (m == MOD_LCTL || saved_mod == MOD_LCTL) {
+	return swap(MOD_LCTL, KC_V, KC_PGDN,  record->event.pressed);
+      }
+      break;
+    }
+    }
+  } else {
+    switch(keycode) {
+    case KC_G: return swap(MOD_LCTL, KC_G, KC_ESC,  record->event.pressed); break;
+    case KC_J: return swap(MOD_LCTL, KC_J, KC_ENT,  record->event.pressed); break;
+    case KC_H: return swap(MOD_LCTL, KC_H, KC_BSPC, record->event.pressed); break;
+    }
   }
   return true;
 }
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
-
+  /*
   switch(id) {
   case 1:
     if (record->event.pressed) {
@@ -127,7 +150,7 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
     }
     break;
   }
-
+  */
   return MACRO_NONE;
 };
 
